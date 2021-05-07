0b23185672f8dcd68e44ab8ea7778cee8098df0e
ryanhill <rhill@ippon.fr>
2021-05-07 10:15:13 -0400

    first

--- src/main/webapp/app/entities/index.tsx
+++ src/main/webapp/app/entities/index.tsx
@@ -4,12 +4,14 @@ import { Switch } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
+import Plan from './plan';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 const Routes = ({ match }) => (
   <div>
     <Switch>
       {/* prettier-ignore */}
+      <ErrorBoundaryRoute path={`${match.url}plan`} component={Plan} />
       {/* jhipster-needle-add-route-path - JHipster will add routes here */}
     </Switch>
   </div>
